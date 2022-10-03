cd44340148d1392e7af9d00e65438c50e0b6d86a
Lucy Heath <lucyheath07@gmail.com>
2022-10-03 16:16:46 +0100

    ye[

---
+++ src/app/helpers/mockAnimals.ts
@@ -0,0 +1,6 @@
+import { Animal} from "../models/animal";
+
+export const ANIMALS: Animal[] =
+[
+    {id: 1, name:'Cow', products:['steak', 'milk'], powerups:[], imagefile:'', baseProduceRate:10, boostedProduceRate:0, maxProduceRate:1000 }
+]
